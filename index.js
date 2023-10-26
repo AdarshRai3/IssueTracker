be8a7348cf6a6b15248d141be9be92c5c092c17a
AdarshRai3 <git config --global user.email>
2023-10-26 22:00:51 +0530

    Commit New Project

--- index.js
+++ index.js
@@ -1,42 +1,9 @@
 import express from 'express';
-import ProjectController from './src/controllers/project_controller.js';
-import path from 'path';
-import expressLayouts from 'express-ejs-layouts';
-import validationMiddleware from './src/middlewares/validation_middleware.js';
 
-const server = express();
-const projectController = new ProjectController;
+const app = express();
 
-server.use(expressLayouts);
-server.use(express.json());
-server.use(express.static('src/views'));
-// parse form data
-server.use(express.urlencoded({extended:true}));
-
-
-// setup view engine settings
-server.set("view engine", "ejs");
-server.set("views",
-path.join(path.resolve(),'src','views')
-);
-
-
-// Create instance of ProductController
-server.get('/',projectController.getProject);
-server.get(
-    '/create-project',
-    projectController.getAddProject
-);
-server.get(
-    '/update-project/:id',
-    projectController.getUpdateProjectView
-);
-server.post(
-    '/',
-    validationMiddleware,
-    projectController.postNewProject
-);
-
-server.listen (3000, (req,res)=>{
-    console.log('Express Server is Up and Running');
-})
+app.get('/',(req,res)=>
+{
+   return res.send("Welcome to Issue Tracker App")
+});
+app.listen(9000);
